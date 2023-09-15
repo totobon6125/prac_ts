ef6ea116e4668702491dcb43591b345a5f15653c
totobon <totobon6125@gmail.com>
2023-09-16 01:03:12 +0900

    1

--- 01.ts
+++ 01.ts
@@ -3,6 +3,12 @@ let str = 'hello';
 
 // TS 문자열 선언
 let str1: string = 'hello';
+
+// TS 숫자 선언
 let num: number = 10;
+
+// TS 배열 선언
 let arr: Array<number> = [1,2,3];
+let items: number[] = [1,2,3];  
+
 let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
